207e9f2d054982036962182044adc36206d1a37e
Caronor <119178135+Caronor@users.noreply.github.com>
2025-02-26 16:34:41 +0100

    Common, Player, Enemy

---
+++ Common/src/main/java/dk/sdu/cbse/common/data/Entity.java
@@ -0,0 +1,67 @@
+package dk.sdu.cbse.common.data;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class Entity implements Serializable {
+    private final UUID ID = UUID.randomUUID();
+
+    private double[] polygonCoordinates;
+    private double x;
+    private double y;
+    private double rotation;
+    private float radius;
+    private String color;
+
+    public UUID getID() {
+        return ID;
+    }
+
+    public double[] getPolygonCoordinates() {
+        return polygonCoordinates;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getRotation() {
+        return rotation;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setPolygonCoordinates(double[] polygonCoordinates) {
+        this.polygonCoordinates = polygonCoordinates;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setRotation(double rotation) {
+        this.rotation = rotation;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ Common/src/main/java/dk/sdu/cbse/common/data/GameData.java
@@ -0,0 +1,9 @@
+package dk.sdu.cbse.common.data;
+
+public class GameData {
+    private int displayWidth = 800;
+    private int displayHeight = 800;
+    private final GameKeys keys = new Gamekeys();
+
+
+}
