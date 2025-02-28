6b44b5aa488d2e375bb5a118ec3da85d7e9541f7
Caronor <119178135+Caronor@users.noreply.github.com>
2025-02-28 22:18:11 +0100

    Asteroid

---
+++ Asteroid/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+import dk.sdu.cbse.common.services.IEntityProcessingService;
+import dk.sdu.cbse.common.services.IGamePluginService;
+
+module Asteroid {
+    requires Common;
+    requires CommonAsteroid;
+}
