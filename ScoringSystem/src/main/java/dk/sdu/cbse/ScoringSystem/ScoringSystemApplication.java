1cf983f2cb734b65ae89bfa4d72d658f065481bd
Caronor <119178135+Caronor@users.noreply.github.com>
2025-05-14 09:53:39 +0200

    ScoringSystem added properly

--- ScoringSystem/src/main/java/dk/sdu/cbse/ScoringSystem/ScoringSystemApplication.java
+++ ScoringSystem/src/main/java/dk/sdu/cbse/ScoringSystem/ScoringSystemApplication.java
@@ -2,13 +2,17 @@ package dk.sdu.cbse.ScoringSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class ScoringSystem {
+public class ScoringSystemApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ScoringSystemApplication.class, args);
 	}
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
