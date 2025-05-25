e354621cd90e197bd5e9a8d67703cb1cc3af4c92
Caronor <119178135+Caronor@users.noreply.github.com>
2025-05-25 22:48:33 +0200

    PlayerControlSystemTest created

---
+++ Player/src/test/java/dk/sdu/cbse/playerSystem/PlayerControlSystemTest.java
@@ -0,0 +1,18 @@
+package dk.sdu.cbse.playerSystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerControlSystemTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void process() {
+    }
+}
